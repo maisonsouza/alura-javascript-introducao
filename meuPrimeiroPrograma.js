c11c7fc5a267191f4e009e9b1e90b053310e68a1
maison.souza@prodam.am.gov.br <Maison1528>
2021-04-06 06:56:49 -0400

    Guardar valores na memória do computador
    Trabalhando com variáveis e console.log

--- meuPrimeiroPrograma.js
+++ meuPrimeiroPrograma.js
@@ -1,10 +1,26 @@
-console.log('Meu Primeiro Programa: trabalhanco com variáveis');
-// Como imprimir valores na tela
+// Utilizando o console com aspas simples
+console.log('Meu Primeiro Programa: trabalhando com console.log e aspas simples');
+
+// Utilizando o console com aspas duplas e sem ponto e vírgula
+console.log("Meu Primeiro Programa: trabalhando com console.log e aspas duplas")
+
+// Declarando e atribuindo variáveis
 const idade = 26;
+
+// Imprimindo variáveis na tela
 console.log(idade)
-// Declaração de variáveis
+
+// Imprimindo um texto
+console.log("Idade como texto")
+
+// Manipulando variáveis
 const idadeSomada = idade + 2;
 console.log(idadeSomada);
-// Operadores aritméticos
+
+// Operadores aritméticos subtração
 const diferenca = idadeSomada - idade;
 console.log(diferenca)
+const produto = 2 * 3
+
+//Concatenando texto e variáveis
+console.log('Produto = ', produto)
